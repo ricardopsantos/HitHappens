02f4368a8c77cf3c00f02e5e741c61e725eaf28f
Ricardo Santos <ricardopsantos@users.noreply.github.com>
2024-09-12 12:08:51 +0100

    dev/V1.0.1 - start working on widget

---
+++ Source/Favorits-Extension/Favorits_ExtensionBundle.swift
@@ -0,0 +1,17 @@
+//
+//  Favorits_ExtensionBundle.swift
+//  Favorits-Extension
+//
+//  Created by Ricardo Santos on 12/09/2024.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct Favorits_ExtensionBundle: WidgetBundle {
+    var body: some Widget {
+        Favorits_Extension()
+        // Favorits_ExtensionLiveActivity()
+    }
+}
