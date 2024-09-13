9aed31850077062506c518c888a0b82514ea005b
Ricardo Santos <ricardopsantos@users.noreply.github.com>
2024-09-13 16:35:20 +0100

    dev/V1.1.0

--- Source/makeProject.sh
+++ Source/makeProject.sh
@@ -11,6 +11,40 @@ displayCompilerInfo() {
 	eval xcode-select --print-path
 }
 
+removePlistFromMainTarget() {
+    # Define variables
+    PROJECT_PATH="HitHappens.xcodeproj"
+    FILE_PATH="Application/Info.plist"
+    TARGET_NAME="HitHappens Dev"
+    PBXPROJ_PATH="$PROJECT_PATH/project.pbxproj"
+
+    # Verify if the file exists
+    if [ ! -f "$PBXPROJ_PATH" ]; then
+        echo "Error: $PBXPROJ_PATH does not exist or is not a regular file."
+        return 1
+    fi
+
+    # Locate the file reference ID
+    FILE_REF_ID=$(grep -B 10 "$FILE_PATH" "$PBXPROJ_PATH" | grep -Eo '^[[:alnum:]]{24}' | head -n 1)
+
+    # Locate the target build phase ID
+    BUILD_PHASE_ID=$(grep -A 10 "$TARGET_NAME" "$PBXPROJ_PATH" | grep -Eo '^[[:alnum:]]{24}' | head -n 1)
+
+    # Remove the file reference from the target's build phases
+    if [ -n "$FILE_REF_ID" ]; then
+        sed -i '' "/$FILE_REF_ID/d" "$PBXPROJ_PATH"
+        echo "Removed file reference $FILE_PATH from target $TARGET_NAME"
+    else
+        echo "File reference for $FILE_PATH not found"
+    fi
+
+    # Optionally, remove build phase if empty
+    if [ -n "$BUILD_PHASE_ID" ]; then
+        sed -i '' "/$BUILD_PHASE_ID/d" "$PBXPROJ_PATH"
+        echo "Removed build phase $BUILD_PHASE_ID if empty"
+    fi
+}
+
 ################################################################################
 
 echo "### Brew"
@@ -158,13 +192,13 @@ xcodegen dump --spec ./XcodeGen/XcodegenMultiConfig.yml --type summary --file ./
 
 ################################################################################
 
+# Use plutil to remove the file from the target's build phases
 echo ""
 echo "↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ WARNING ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓"
 echo "↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ WARNING ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓"
 echo ""
 echo " Dont forget to:"
 echo "  - For Main target: Remove Info.plist from target Membership!!"
-echo "  - For UnitTests target: Set Host Application to Main target"
 echo ""
 echo "↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ WARNING ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑"
 echo "↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ WARNING ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑"
@@ -172,5 +206,9 @@ echo ""
 echo " ╔═══════════════════════╗"
 echo " ║ Done! You're all set! ║"
 echo " ╚═══════════════════════╝"
+
+#chmod u+rw HitHappens.xcodeproj/project.pbxproj
+#removePlistFromMainTarget
+#sed -i '' "/Application\/Info.plist/d" HitHappens.xcodeproj
 
 open HitHappens.xcodeproj
