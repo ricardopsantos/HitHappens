150caefb21e809177cab61562a7c9adf7055f519
Ricardo Santos <ricardopsantos@users.noreply.github.com>
2024-09-17 16:15:44 +0100

    de/V1.2.0 - fixed leak on audio player

--- Source/Application/7.Utils/Extensions/E.SoundEffect+Extensions.swift
+++ Source/Application/7.Utils/Extensions/E.SoundEffect+Extensions.swift
@@ -13,37 +13,73 @@ import Domain
 
 public extension SoundEffect {
     static func with(localized: String) -> SoundEffect? {
-        SoundEffect.allCases.filter { $0.localized == localized }.first
+        SoundEffect.allCases.first { $0.localized == localized }
     }
 
     func play() {
-        let soundPlayer = SoundPlayer()
-        soundPlayer.configureAudioSession()
-        soundPlayer.playSound(self)
+        SoundPlayer.shared.playSound(self)
     }
 
-    class SoundPlayer {
-        static var audioPlayer: AVAudioPlayer?
-        func configureAudioSession() {
-            do {
-                try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default, options: [])
-                try AVAudioSession.sharedInstance().setActive(true)
-            } catch {
-                DevTools.Log.error("Failed to set audio session category: \(error)", .generic)
-            }
+    class SoundPlayer: NSObject, AVAudioPlayerDelegate {
+        static let shared = SoundPlayer()
+        private var audioPlayer: AVAudioPlayer?
+        private var audioSession: AVAudioSession?
+
+        override private init() {
+            super.init()
         }
 
         func playSound(_ sound: SoundEffect) {
+            if audioPlayer != nil {
+                stopSound()
+            }
+            guard audioPlayer == nil, audioSession == nil else { return }
             guard sound != .none else { return }
             guard let soundURL = Bundle.main.url(forResource: sound.rawValue, withExtension: nil) else {
                 DevTools.Log.error("Sound file not found: \(sound.rawValue)", .generic)
                 return
             }
+
             do {
-                SoundPlayer.audioPlayer = try AVAudioPlayer(contentsOf: soundURL)
-                SoundPlayer.audioPlayer?.play()
+                activateAudioSession() // Ensure session is configured before playback
+                audioPlayer = try AVAudioPlayer(contentsOf: soundURL)
+                audioPlayer?.delegate = self
+                audioPlayer?.play()
             } catch {
-                DevTools.Log.error(error, .generic)
+                DevTools.Log.error("Failed to play sound: \(error)", .generic)
+                deactivateAudioSession() // Clean up if playback fails
+            }
+        }
+
+        func stopSound() {
+            audioPlayer?.stop()
+            deactivateAudioSession() // Deactivate session when sound is stopped
+        }
+
+        public func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
+            deactivateAudioSession()
+        }
+
+        private func activateAudioSession() {
+            guard audioSession == nil else { return } // Avoid re-activation
+            do {
+                audioSession = AVAudioSession.sharedInstance()
+                try audioSession?.setCategory(.playback, mode: .default, options: [])
+                try audioSession?.setActive(true)
+            } catch {
+                DevTools.Log.error("Failed to set audio session category: \(error)", .generic)
+            }
+        }
+
+        private func deactivateAudioSession() {
+            defer {
+                audioSession = nil
+                audioPlayer = nil
+            }
+            do {
+                try audioSession?.setActive(false)
+            } catch {
+                DevTools.Log.error("Failed to deactivate audio session: \(error)", .generic)
             }
         }
     }
